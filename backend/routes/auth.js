2b961f2d54024dc612821811c9fc338985f88a50
Yash-Sen <yashsen0402@gmail.com>
2024-05-27 00:00:54 +0530

    User Route Updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
-    obj = {
-        "name": "yash",
-        "email": "EMAIL",
-    }
-    res.json(obj)
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
 })
 
 module.exports = router;
