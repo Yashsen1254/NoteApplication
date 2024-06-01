7598dc2967ee76a735752822d0c76259c9344960
Yash-Sen <yashsen0402@gmail.com>
2024-06-01 23:45:18 +0530

    Password Comparing Done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,4 +49,39 @@ router.post(
   }
 );
 
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // Check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try {
+      let user= await User.findOne({email});
+      if(!user) {
+        return res.status(400).json({errors:"Invalid Detail"})
+      }
+      const passCompare = await bcrypt.compare(password,user.password);
+      if(!passCompare) {
+        return res.status(400).json({errors:"Invalid Detail"})
+      }
+      const data = {
+        user:{
+          id : user.id,
+        }
+      }
+      const authtoken = jwt.sign(data,JWT_SEC);
+      res.json({authtoken});
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ errors: "Server Error" });
+    }
+  })
+
 module.exports = router;
