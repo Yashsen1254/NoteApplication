7a654a29943939e5a45f79c8b060d5f95f47d14a
Yash-Sen <yashsen0402@gmail.com>
2024-06-02 22:23:08 +0530

    FetchUser Updated

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,18 +1,19 @@
-var jwt = require('jsonwebtoken');
-const JWT_SEC = "secret";
+const jwt = require('jsonwebtoken');
+let jwt_secret = "secret";
 
-const fetchuser = (req,res,next) => {
+const fetchuser = (req,res,next) => 
+    {
     const token = req.header('auth-token');
     if(!token){
-        return res.status(401).json({msg: 'No token, authorization denied'});
+        res.status(401).send({error: 'No token, authorization denied'});
     }
     try {
-        const data = jwt.verify(token,JWT_SEC);
+        const data = jwt.verify(token,jwt_secret);
         req.user = data.user;
         next();
     } catch (error) {
-        res.status(401).json({msg: 'Token is not valid'});
+        res.status(401).send({error: 'Token is not valid'});
     }
 }
 
-module.export = fetchuser;
+module.exports = fetchuser;
