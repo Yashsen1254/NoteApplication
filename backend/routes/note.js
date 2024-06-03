f4c9d074eab3f071e3d107e47cbdf1c6ad0c8997
Yash-Sen <yashsen0402@gmail.com>
2024-06-03 19:29:54 +0530

    Fetch notes added

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require('../models/Note');
+const fetchuser = require('../middleware/fetchuser');
 
-router.get('/', (req, res) => {
-    res.json([])
+// get all notes
+router.get('/fetchallnotes',fetchuser ,async (req, res) => {
+    const notes = await Notes.find({user: req.user.id});
+    res.json(notes);
 })
 
 module.exports = router;
