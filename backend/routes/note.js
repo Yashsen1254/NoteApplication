1b71d89f3dadc18999d9a4bc79a18f35a1df5061
Yash-Sen <yashsen0402@gmail.com>
2024-06-03 20:21:36 +0530

    Update Endpoint Created

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -7,7 +7,7 @@ const fetchuser = require("../middleware/fetchuser");
 // get all notes
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
-    const notes = await Notes.find({ user: req.user.id });
+    const notes = await Note.find({ user: req.user.id });
     res.json(notes);
   } catch (error) {
     console.log(error);
@@ -46,5 +46,27 @@ router.post(
     }
   }
 );
+
+// update note
+router.put("/updatenotes/:id", fetchuser, async (req, res) => {
+    try {
+    const {title,description,tag} = req.body;
+    
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    const note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not found")};
+    if(note.user.toString() !== req.user.id){return res.status(401).send("Not authorized")};
+    await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+    res.json({note});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errors: "Server Error" });
+  }
+  })
 
 module.exports = router;
